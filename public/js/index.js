f9501561f66ee2f04945eab96ff4b9b93d56aa3c
Jerry <exnxs@hotmail.com>
2017-07-22 11:45:11 -0400

    Add newMessage and createMessage event

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+var socket = io();
+
+socket.on('connect', function() {
+  console.log('connected to server');
+
+  // socket.emit('createEmail', {
+  //     from: 'jerry@test.com',
+  //     text: 'Yah!'
+  //   });
+  // });
+
+  socket.emit('createMessage', {
+      from: 'Me',
+      text: 'Yah!'
+    });
+  });
+
+socket.on('disconnect', function() {
+  console.log('Server disconnected');
+});
+
+socket.on('newMessage', function(message) {
+
+  console.log('new message: ', message);
+});
--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=> {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'John',
+    test: 'Hey, sup!',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (newMessage) => {
+    console.log(newMessage);
+  });
+
   socket.on('disconnect', () => {
     console.log('client disconnected');
   })
